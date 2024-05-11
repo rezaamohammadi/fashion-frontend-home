f93c2286bfb56a4fd6f82138c54b55c687b1ebc0
= <=>
2024-05-11 02:46:35 -0700

    little edit

--- src/pages/front/KidBoy.js
+++ src/pages/front/KidBoy.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import { Swiper, SwiperSlide } from 'swiper/react';
-import ProductCard from "../../components/carts/ProductCard";
+import ProductCard from "../../components/cards/ProductCard";
 export function loader(data) {
     return 0
 }
