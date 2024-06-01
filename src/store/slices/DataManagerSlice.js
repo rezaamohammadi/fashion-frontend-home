c9e02f9c21beed69327b15822be4a060577cb124
= <=>
2024-06-01 03:11:59 -0700

    add wizard css

---
+++ src/store/slices/DataManagerSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const DataManagerSlice = createSlice({
+  name: "",
+  initialState: {
+   
+  },
+  reducers: {},
+});
+
+
+
+export default DataManagerSlice.reducer;
